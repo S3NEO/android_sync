27f0d885f000b5421940fa443aefd29f7e1e29e1
pythonlimited <leonardomeitz@gmail.com>
2019-07-13 10:38:53 +0000

    fix jenkins failures

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ source build/envsetup.sh
 breakfast s3ve3g
 brunch s3ve3g
 rm -rf /var/www/html/s3ve3g/samsung/*
-cp -rf out/target/product/s3ve3g/lineage-*.zip var/www/html/s3ve3g/samsung/
+cp -rf out/target/product/s3ve3g/lineage-*.zip /var/www/html/s3ve3g/samsung/
 bash ota.sh
 echo "all done going in for a reboot to clear caches...."
-reboot
